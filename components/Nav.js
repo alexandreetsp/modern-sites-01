951a550ed0bb4c4b8b96d28a59605cde7d57197b
unknown <alexandresousa02@seduc.pi.gov.br>
2023-10-08 23:32:55 -0300

    menu-and-home

--- components/Nav.js
+++ components/Nav.js
@@ -6,28 +6,58 @@ import {
   HiRectangleGroup,
   HiChatBubbleBottomCenterText,
   HiEnvelope,
-} from 'react-icons/hi2';
+} from "react-icons/hi2";
 
 // nav data
 export const navData = [
-  { name: 'home', path: '/', icon: <HiHome /> },
-  { name: 'about', path: '/about', icon: <HiUser /> },
-  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  { name: "home", path: "/", icon: <HiHome /> },
+  { name: "about", path: "/about", icon: <HiUser /> },
+  { name: "services", path: "/services", icon: <HiRectangleGroup /> },
+  { name: "work", path: "/work", icon: <HiViewColumns /> },
   {
-    name: 'testimonials',
-    path: '/testimonials',
+    name: "testimonials",
+    path: "/testimonials",
     icon: <HiChatBubbleBottomCenterText />,
   },
   {
-    name: 'contact',
-    path: '/contact',
+    name: "contact",
+    path: "/contact",
     icon: <HiEnvelope />,
   },
 ];
 
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+
+  return (
+    <nav className="grid grid-cols xl:place-items-center w-full fixed bottom-0 xl:w-16 xl:right-4 xl:top-0 xl:h-screen ">
+      <div className="flex xl:flex-col w-full justify-between py-10 px-4 bg-white/10 text-3xl xl:rounded-full xl:px-6 xl:gap-y-10 xl:text-xl">
+        {navData.map((link, index) => {
+          return (
+            <Link
+              className={`${
+                link.path === pathname && "text-accent"
+              } hover:text-accent group transition-all duration-300`}
+              href={link.path}
+              key={index}
+            >
+              <div className="absolute pr-14 right-4 hidden xl:group-hover:flex">
+                <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
+                  <div className="text-[12px] leading-none capitalize">{link.name}</div>
+                </div>
+                <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-1 absolute right-[50px] top-[6px]"></div>
+              </div>
+              <div>{link.icon}</div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,35 @@
+import Image from "next/image";
+
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
+import Avatar from "../components/Avatar";
+
+import { motion } from "framer-motion";
+
+import { fadeIn } from "../variants";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full">
+      {/*Home*/}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 ">
+        <div className="text-center flex flex-col xl:text-left  justify-center h-full container mx-auto ">
+          <h1 className="h1">
+            Transforming Ideas <br/> Into {' '}
+            <span className="text-accent">Digital Reality</span>
+          </h1>
+          <p className="max-w-sm xl:max-w-xl xl:mx-0 mb-10 mx-auto">
+            First of all thanks for the tutorial! Having some issues though in
+            which I spent way too much time on lol, specifically, I've been
+            facing one where my header 
+          </p>
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn></ProjectsBtn>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
